f594466d50008cffd3dfb2060c66f85a2ed9a657
Oliver <oliverbathurst12345@gmail.com>
2018-02-15 13:57:08 +0000

    Random commit

--- src/uk/ac/reading/oliver/bathurst/MailTool.java
+++ src/uk/ac/reading/oliver/bathurst/MailTool.java
@@ -4,9 +4,9 @@ import jcsp.lang.One2OneChannel;
 import jcsp.lang.Parallel;
 
 class MailTool implements CSProcess{
-    private final One2OneChannel unitChannel = new One2OneChannel();
 
     MailTool(One2OneChannel eticket){
+        One2OneChannel unitChannel = new One2OneChannel();
         new Parallel(new CSProcess[]{new ETicketMailBag(eticket, unitChannel), new ETicketGUI(unitChannel)}).run();
     }
 
